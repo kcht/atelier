c46d26facf49cf0e02418b264dc33732b3da0481
Katarzyna Chachlowska <katarzyna.chachlowska@netguru.pl>
2017-11-22 19:05:10 +0100

    Extract similar books

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,17 @@
-class Category < ApplicationRecord
-  has_many :books
+class Category < ActiveYaml::Base
+  set_root_path 'db/data'
+
+  def books
+    ::Book.where(category_id: self.id)
+  end
+
+  def similar_books
+    ::Book.where(category_id: similar_category_ids)
+  end
+
+  private
+
+  def similar_category_ids
+    similar_categories.map{ |category| Category.find_by(name: category).id }
+  end
 end
