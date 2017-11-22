e60d28870c2e649ba78255aa1e9362fce500aa8f
Katarzyna Chachlowska <katarzyna.chachlowska@netguru.pl>
2017-11-22 19:12:47 +0100

    Drop categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102211338) do
+ActiveRecord::Schema.define(version: 20171122181015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,10 +48,6 @@ ActiveRecord::Schema.define(version: 20171102211338) do
     t.index ["author_id"], name: "index_books_on_author_id"
     t.index ["category_id"], name: "index_books_on_category_id"
     t.index ["publisher_id"], name: "index_books_on_publisher_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
   end
 
   create_table "publishers", force: :cascade do |t|
